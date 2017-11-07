36c2e429379f86f0d89f0fa7665aec86a403eda0
Tom Whiston <tomw@nine.ch>
2017-11-07 20:31:49 +0100

    lint recursively

--- context/codacy/scripts/test.sh
+++ context/codacy/scripts/test.sh
@@ -3,6 +3,6 @@
 echo "---> Run Golang Tests"
 /go/bin/goverage -v -coverprofile=coverage.out ./...
 echo "---> Run MetaLinter Tests"
-/go/bin/gometalinter --exclude \w*_test\w*
+/go/bin/gometalinter --exclude \w*_test\w* ./...
 echo "---> Export Golang Test Coverage to Codacy"
 godacov -t ${CODACY_TOKEN} -r ./coverage.out -c ${CI_COMMIT_ID}
