dd7ec26a3f5c7c0790403cabb6a2d151febb6e36
Tom Whiston <tomw@nine.ch>
2017-11-07 19:40:57 +0100

    add codacy coverage context

---
+++ context/codacy/scripts/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "---> Run Golang Tests"
+/go/bin/goverage -v -coverprofile=coverage.out ./...
+/go/bin/gometalinter --exclude \w*_test\w*
+echo "---> Export Golang Test Coverage to Codacy"
+godacov -t ${CODACY_TOKEN} -r ./coverage.out -c ${CI_COMMIT_ID}
