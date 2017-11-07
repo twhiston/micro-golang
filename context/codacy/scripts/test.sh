dc0ecd4b292850a39f1d3ade12edfbf5cc4eec25
Tom Whiston <tomw@nine.ch>
2017-11-07 20:42:01 +0100

    trap exit signals to post coverage and return the trapped exit code

--- context/codacy/scripts/test.sh
+++ context/codacy/scripts/test.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
+postCoverage() {
+    echo "---> Export Golang Test Coverage to Codacy"
+    godacov -t ${CODACY_TOKEN} -r ./coverage.out -c ${CI_COMMIT_ID}
+    exit $?
+}
+
+trap "postCoverage" INT TERM EXIT
+
 echo "---> Run Golang Tests"
 /go/bin/goverage -v -coverprofile=coverage.out ./...
+
 echo "---> Run MetaLinter Tests"
 /go/bin/gometalinter --exclude \w*_test\w* ./...
-echo "---> Export Golang Test Coverage to Codacy"
-godacov -t ${CODACY_TOKEN} -r ./coverage.out -c ${CI_COMMIT_ID}
