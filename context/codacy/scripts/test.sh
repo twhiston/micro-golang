39829e9f2090bc340c7ace3c7f49a15372d0aaaa
Tom Whiston <tomw@nine.ch>
2017-11-07 20:12:20 +0100

    make linting explicit

--- context/codacy/scripts/test.sh
+++ context/codacy/scripts/test.sh
@@ -2,6 +2,7 @@
 
 echo "---> Run Golang Tests"
 /go/bin/goverage -v -coverprofile=coverage.out ./...
+echo "---> Run MetaLinter Tests"
 /go/bin/gometalinter --exclude \w*_test\w*
 echo "---> Export Golang Test Coverage to Codacy"
 godacov -t ${CODACY_TOKEN} -r ./coverage.out -c ${CI_COMMIT_ID}
