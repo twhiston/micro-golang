897735241fe6ecbf9bb001f5f0ddaf23e501e429
Tom Whiston <tomw@nine.ch>
2018-01-13 14:16:30 +0100

    add install script so dep can be used, add tests for this

---
+++ context/prod/scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+
+echo "---> Installing Application"
+if [ -e Gopkg.lock ]; then
+    echo "---> Using dep ensure"
+    /go/bin/dep ensure
+    go install
+else
+    echo "---> Using go get"
+    go get -v -t app
+fi
